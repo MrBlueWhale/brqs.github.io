de7bd74299de5cb5a129c2a1ef307a450fcb0326
brqs <3447851674@qq.com>
2020-11-09 21:29:40 +0800

    Site updated: 2020-11-09 21:29:39

---
+++ status/js/config.js
@@ -0,0 +1,10 @@
+// array of Monitor-specific API keys or Main API key to list all monitors
+var __apiKeys = [
+    'm786261624-7adf4da1b8397d488fb874f9', //凡尘纪
+	'm786035924-5a0066185d17e96d40f1e33d', //今生主博客
+	
+	
+];
+
+// refresh interval (in seconds)
+var __refresh = 120;
