d2a44a0ae7505eb3fd833891a6df9d339e76c894
brqs <3447851674@qq.com>
2021-01-05 19:18:12 +0800

    Site updated: 2021-01-05 19:18:11

--- js/q.js
+++
@@ -1,3 +0,0 @@
-var jsref51la = document.getElementById('la_21036417');
-var content51la = '<a href="//www.51.la/?comId=21036417" target="_blank">总访问量&nbsp;17,005，本月访问量&nbsp;81，昨日访问量&nbsp;60，今日访问量&nbsp;21，当前在线&nbsp;4</a>';
-if (jsref51la) {jsref51la.innerHTML = content51la;} else {document.write(content51la);}
